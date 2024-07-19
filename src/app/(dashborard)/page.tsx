675fb34d26fabf82433739bfeb79546776841af1
qianyu@MacBook <qianyu_work@icloud.com>
2024-07-19 10:03:14 +0800

    feat: Dashboard layout
    新增仪表盘页面框架

--- src/app/(dashborard)/page.tsx
+++ src/app/(dashborard)/page.tsx
@@ -1,12 +1,9 @@
 import {UserButton} from "@clerk/nextjs";
 
-export default function Home() {
+export default function DashboardPage() {
   return (
     <div className="flex flex-col gap-y-4">
-      <div>This is a ...</div>
-      <div>
-        <UserButton />
-      </div>
+      <div>This is a body</div>
     </div>
   );
 }
