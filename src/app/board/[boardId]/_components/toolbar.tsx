89986f0395d3f734df3496ee294759a79426bcde
qianyu@MacBook <qianyu_work@icloud.com>
2024-07-25 19:28:15 +0800

    feat: Room Toolbar
    完善工具栏Icon、状态

--- src/app/board/[boardId]/_components/toolbar.tsx
+++ src/app/board/[boardId]/_components/toolbar.tsx
@@ -1,15 +1,20 @@
+import {ToolButton} from "@/app/board/[boardId]/_components/tool-button";
+import {Circle, MousePointer2, Pen, Redo2, Square, StickyNote, Type, Undo} from "lucide-react";
+
 export const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div>Pencil</div>
-        <div>Pencil</div>
-        <div>Pencil</div>
-        <div>Pencil</div>
+        <ToolButton label="选择" icon={MousePointer2} onClick={()=>{}} isActive={true}/>
+        <ToolButton label="文本" icon={Type} onClick={()=>{}} isActive={false}/>
+        <ToolButton label="便利贴" icon={StickyNote} onClick={()=>{}} isActive={false}/>
+        <ToolButton label="矩形" icon={Square} onClick={()=>{}} isActive={false}/>
+        <ToolButton label="椭圆形" icon={Circle} onClick={()=>{}} isActive={false}/>
+        <ToolButton label="画笔" icon={Pen} onClick={()=>{}} isActive={false}/>
       </div>
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <div>Undo</div>
-        <div>Redo</div>
+        <ToolButton label="撤销" icon={Undo} onClick={()=>{}} isDisabled={true}/>
+        <ToolButton label="重做" icon={Redo2} onClick={()=>{}} isDisabled={true}/>
       </div>
     </div>
   )
